e99793495254643d1e6c0db67e4d056ca8a3f9e0
lamnguyendnvncn <lamnguyendnvncn@gmail.com>
2023-01-27 00:01:38 -0500

    update

--- JS/index.js
+++ JS/index.js
@@ -74,3 +74,134 @@ function toString(a,b){
 // console.log(sum(2,3))
 // console.log(toString(2,3))
 
+// Different types of function
+// above are declaration function: can be called before declaration
+
+// expression function: cannot be called before declaration
+var showMessage2 = function() {
+    console.log(arguments)
+}
+
+var myObject = {
+    myFunction: function(){
+        console.log(arguments)
+    }
+}
+
+setTimeout(function(){
+
+});
+
+// String
+var fullName = 'Lam Nguyen';
+var fName = new String('Nguyen Thanh Lam')
+var quote = "He said: \"Hello world!\"";
+var lengthName = fullName.length
+var firstName = 'Lam';
+var lastName = 'Nguyen';
+var myName = `${firstName} ${lastName}`
+
+// Working with string
+var myString = '  My name is Lam Nguyen Lam    '
+
+// console.log(myString.length)
+// console.log(myString.indexOf('Lam',12)) //Equal -1 if not found, second parameter is start point
+// console.log(myString.lastIndexOf('Lam'))
+// console.log(myString.slice(myString.indexOf('Lam'),myString.length))
+// console.log(myString.slice(-3))
+// console.log(myString.replace('Nguyen Lam','Nguyen Thanh'))
+// console.log(myString.replace(/Lam/g,''))
+// console.log(myString.toUpperCase())
+// console.log(myString.toLowerCase())
+// console.log(myString.trim()) //remove all redundant spaces at 2 end
+// console.log(myString.trim().split(" "))
+// console.log(typeof myString.charAt('33'))
+
+// Number in JS
+var age = 20;
+var PI=3.14;
+
+var otherNum = new Number(23);
+// console.log(typeof age, typeof otherNum)
+
+// console.log(age.toString())
+// console.log(isNaN(20/'abc')) //NaN stands for invalid result
+
+// console.log(PI.toFixed(1)) //
+// console.log(typeof PI === 'number')
+// console.log(typeof NaN === "number") //NaN is still considered number
+
+var numbers = ['1','2','3','4','5']
+// console.log(typeof numbers)
+// console.log(numbers.length)
+// for (var i=0;i<numbers.length;i++) {
+//     console.log(numbers[i])
+// }
+// console.log(Array.isArray(numbers)) //check if a object is array
+
+// Array methods
+var numbers=['js','php','ruby']
+// console.log(numbers.toString())
+// console.log(numbers.join('-'))
+// console.log(numbers.pop()) //remove last element and return that element
+// console.log(numbers.push('java')) // add at the end of array
+// console.log(numbers)
+// console.log(numbers.shift())//remove first element and return that element
+// console.log(numbers.unshift('javascript')) // add at the begin of array
+// var array=[1,2,3,4,6,7,8,12]
+// var sub=[12,13,14]
+// console.log(array.splice(2,3)) //remove from index 2, remove 3 elements, return the removed elements
+// console.log(array.splice(1,0,22,33,44)) //after the number of delete, it's the items you want to add at the start position
+// console.log(array)
+// console.log(array.concat(sub)) //add array sub to array
+// console.log(array.concat(sub).slice(2,8))
+// console.log(array)
+// console.log(array.slice(-1))
+
+
+// Object
+var addressKey = 'address'
+var myInfo = {
+    name: 'Lam Nguyen',
+    age: 20,
+    [addressKey]: 'Tampa, FL',
+    printInfo: function(){
+        console.log([this.name,this.age,this.address].join(', '))
+    }
+}
+// myInfo.printInfo()
+myInfo.major = "Computer Engineering"
+myInfo.printInfo = function() {
+    console.log([this.name,this.age,this.address,this.major].join(', '))
+
+}
+var myKey='name';
+// console.log(myInfo[myKey])
+delete myInfo.age
+// myInfo.printInfo()
+
+
+// Object constructor
+function User(firstName, lastName, avatar) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.avatar = avatar;
+    this.getName = function() {
+        return `${firstName} ${lastName}`
+    }
+}
+
+var teacher = new User('Lam','Nguyen','cat')
+var student = new User('dave','ng','dog')
+// console.log(teacher)
+// console.log(student)
+// console.log(teacher.constructor === User)
+teacher.title = 'Teacher at home'
+student.comment = 'Study at home'
+// console.log(teacher)
+// console.log(student)
+// console.log(teacher.getName())
+
+// Object Prototype
+
+
