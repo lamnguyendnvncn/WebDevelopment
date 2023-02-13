8f6b65e45d991c2108e43c4103158a841fac5f0b
Lam Nguyen <lamnguyendnvncn@gmail.com>
2023-02-13 12:42:46 -0500

    update

---
+++ JS/extra.js
@@ -0,0 +1,24 @@
+// JSON
+
+// A data format
+// Javascript Object Notation
+// Data type can be expressed in JSON: number, boolean, null, array, object
+
+// Stringify and Parse
+
+// var json = '["Javascript","PHP"]';
+// var json = '{"name":"Son dang","age":18}';
+// var jsonNumber = '1';
+
+// console.log(JSON.parse(json));  
+// console.log(JSON.stringify({'name': 'Son Dang','age':18}))
+
+
+// Promise
+// setTimeout(() => {
+//     console.log(1);
+// }, 1000);
+
+// console.log(2);
+
+
